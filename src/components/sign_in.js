ea8671f012bdece0a9b013335f0cc01463f1185e
David Ahn <dja1990@gmail.com>
2018-09-17 15:12:12 -0700

    input state challege complete

---
+++ src/components/sign_in.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class SigninForm extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            form: {
+                email: '',
+                password: '',
+            }
+        }
+    }
+
+    handleFormSubmit(event){
+        event.preventDefault();
+        console.log('Called handleFormSubmit', this.state.form);
+
+        const newState= {
+            form: {
+                email: '',
+                password: '',
+            }
+        }
+        this.setState(newState);
+    }
+
+    handleChange(event){
+        const { name, value } = event.target;
+
+        const { form } = this.state;
+
+        this.setState({form: {...form ,[name]: value}});
+    }
+
+    render(){
+        const { email, password } = this.state.form;
+
+        return (
+            <form onSubmit={(event) => { this.handleFormSubmit(event) }}>
+                <div>
+                    <label>Email: </label>
+                    <input type="email" value={email} name="email" onChange={ (event) => this.handleChange(event)} autoComplete="off"/>
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="password" value={password} name="password" onChange={ (event) => this.handleChange(event)}/>
+                </div>
+                <button>Sign In</button>
+            </form>
+        )
+    }
+}
+
+export default SigninForm;
